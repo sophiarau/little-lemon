6a8a60b30e4f2135f04dce9cf60c15d2ba503640
Sophia Rau <sophiarau@Sophias-MacBook-Air.local>
2025-04-22 19:23:10 -0400

    adding pics

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import MenuCard from './menucard';
+import lemon from '/assets/lemon dessert.jpg';
+import greekSalad from '/assets/greek salad.jpg';
+import bruschetta from '/assets/bruschetta.jpg';
+import food from '../assets/restauranfood.jpg';
+
 
 function Main() {
   return (
@@ -7,30 +13,10 @@ function Main() {
         <div className="highlights">
           <h1>Our Specials</h1>
           <div className="highlights-container">
-            <div className="menucard">
-              <h2>Greek Salad</h2>
-              {/* <img src={salad} alt="Greek Salad" /> */}
-              <p className="price"></p>
-              <p>Our Greek salad is a refreshing mix of fresh vegetables, feta cheese, and olives.</p>
-            </div>
-            <div className="menucard">
-              <h2>Greek Salad</h2>
-              {/* <img src={salad} alt="Greek Salad" /> */}
-              <p className="price"></p>
-              <p>Our Greek salad is a refreshing mix of fresh vegetables, feta cheese, and olives.</p>
-            </div>
-            <div className="menucard">
-              <h2>Greek Salad</h2>
-              {/* <img src={salad} alt="Greek Salad" /> */}
-              <p className="price"></p>
-              <p>Our Greek salad is a refreshing mix of fresh vegetables, feta cheese, and olives.</p>
-            </div>
-            <div className="menucard">
-              <h2>Greek Salad</h2>
-              {/* <img src={salad} alt="Greek Salad" /> */}
-              <p className="price"></p>
-              <p>Our Greek salad is a refreshing mix of fresh vegetables, feta cheese, and olives.</p>
-            </div>
+            <MenuCard name="dish1" price="12.00" description="this is a description" image={bruschetta} className="menucard card1"/>
+            <MenuCard name="dish2" price="22.00" description="this is a different description" image={lemon} className="menucard card2"/>
+            <MenuCard name="danny" price="priceless" description="this is a great description" image={greekSalad} className="menucard card3"/>
+            <MenuCard name="danny" price="so much money" description="sexiest man alive" image={food} className="menucard card4"/>
           </div>
           </div>
         <div className="testimonials"></div>
---
+++ src/components/menucard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MenuCard({ name, price, description, image }) {
+  return (
+    <div className="menucard">
+      <h2>{name}</h2>
+      {<img src={image} alt="Greek Salad" width="200px"/>}
+      <p className="price">${price}</p>
+      <p>{description}</p>
+    </div>
+  );
+}
+export default MenuCard;
