c016f9e2d2686918f814fa728f1625bf4806f914
Sophia Rau <sophiarau@Sophias-MacBook-Air.local>
2025-04-26 13:18:47 -0400

    lots of random stuff

--- src/components/main.js
+++ src/components/main.js
@@ -4,6 +4,7 @@ import lemon from '../assets/lemondessert.jpg';
 import greekSalad from '../assets/greeksalad.jpg';
 import bruschetta from '../assets/bruchetta.jpeg';
 import food from '../assets/restauranfood.jpg';
+import basket from '../assets/basket.jpeg';
 
 
 function Main() {
@@ -11,7 +12,13 @@ function Main() {
     <main>
       <div className="main">
         <div className="highlights">
-          <h1>Our Specials</h1>
+          <div className="highlights-header">
+            <h1>OUR SPECIALS</h1>
+            <div className="highlights-header-button">
+              <img src={basket} alt="Cart icon" width="48px" height="48px"/>
+            <button className="order-button">Order Online</button>
+            </div>
+          </div>
           <div className="highlights-container">
             <MenuCard name="dish1" price="12.00" description="this is a description" image={bruschetta} className="menucard card1"/>
             <MenuCard name="dish2" price="22.00" description="this is a different description" image={lemon} className="menucard card2"/>
