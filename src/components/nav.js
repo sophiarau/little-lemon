293f01e4de30aa32d419ced5e3ae8cf539ea19d2
Sophia Rau <sophiarau@Sophias-MacBook-Air.local>
2025-04-28 13:58:56 -0400

    booking files

--- src/components/nav.js
+++ src/components/nav.js
@@ -12,7 +12,7 @@ function Nav() {
           <li><a href="/about">About</a></li>
           <li><a href="/menu">Menu</a></li>
           <li><a href="/reservations">Reservations</a></li>
-          <li><a href="/order">Order Online</a></li>
+          <li><a href="/src/components/BookingPage.js">Order Online</a></li>
           <li><a href="/login">Login</a></li>
       </ul>
     </nav>
